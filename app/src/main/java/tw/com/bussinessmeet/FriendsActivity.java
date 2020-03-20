6fbfe95c8dd386b7a03cf0170d78d39dd58b19eb
Alice Lee <alice880405@gmail.com>
2020-03-20 21:16:00 +0800

    modify navbar color and recycleview hight

--- app/src/main/java/tw/com/bussinessmeet/FriendsActivity.java
+++ app/src/main/java/tw/com/bussinessmeet/FriendsActivity.java
@@ -20,28 +20,32 @@ public class FriendsActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         //Set Home
         bottomNavigationView.setSelectedItemId(R.id.menu_friends);
-        //Perform ItemSelectedListener
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.menu_home:
-                        startActivity(new Intent(getApplicationContext()
-                                ,SelfIntroductionActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.menu_search:
-                        startActivity(new Intent(getApplicationContext()
-                                ,SearchActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.menu_friends:
-                        return true;
-                }
-                return false;
-            }
-        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    //Perform ItemSelectedListener
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.menu_home:
+                    startActivity(new Intent(getApplicationContext()
+                            ,SelfIntroductionActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.menu_search:
+                    startActivity(new Intent(getApplicationContext()
+                            ,SearchActivity.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                case R.id.menu_friends:
+                    return true;
+            }
+            return false;
+        }
+    });
 
 
 }
