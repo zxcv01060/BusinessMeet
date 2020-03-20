459fb093b862d096f94dc0e1a96e0478a30c687c
Alice Lee <alice880405@gmail.com>
2020-03-20 21:53:52 +0800

    add friends_intro and modify self_intro photo id

---
+++ app/src/main/java/tw/com/bussinessmeet/FriendsIntroductionActivity.java
@@ -0,0 +1,13 @@
+package tw.com.bussinessmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendsIntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_introduction);
+    }
+}
