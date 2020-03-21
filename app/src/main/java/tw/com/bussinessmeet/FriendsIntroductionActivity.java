d0d3b09ace63824ae154f67a9d4d1065f5502329
Alice Lee <alice880405@gmail.com>
2020-03-21 14:03:16 +0800

    notofication verson1.0

--- app/src/main/java/tw/com/bussinessmeet/FriendsIntroductionActivity.java
+++ app/src/main/java/tw/com/bussinessmeet/FriendsIntroductionActivity.java
@@ -3,6 +3,7 @@ package tw.com.bussinessmeet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FriendsIntroductionActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class FriendsIntroductionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.friends_introduction);
+
+        TextView textView = findViewById(R.id.friends_name);
+        String message = getIntent().getStringExtra("message");
+        textView.setText(message);
     }
 }
--- app/src/main/java/tw/com/bussinessmeet/NotificationActivity.java
+++ app/src/main/java/tw/com/bussinessmeet/NotificationActivity.java
@@ -1,14 +1,54 @@
 package tw.com.bussinessmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    Button btNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
+
+        btNotification = findViewById(R.id.bt_notification);
+
+        btNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "This is a notific.";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                        NotificationActivity.this
+                )
+                        .setSmallIcon(R.drawable.ic_insert_comment_black_24dp)
+                        .setContentText("New Notification")
+                        .setContentText(message)
+                        .setAutoCancel(true);
+
+                Intent intent = new Intent(NotificationActivity.this,
+                        FriendsIntroductionActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("message",message);
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(NotificationActivity.this,
+                        0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager = (NotificationManager)getSystemService(
+                        Context.NOTIFICATION_SERVICE
+                );
+
+                notificationManager.notify(0,builder.build());
+            }
+        });
     }
 }
