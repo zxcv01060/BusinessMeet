acefc8f9ac574e3e3e8af2791e3771463cbe1853
Alice Lee <alice880405@gmail.com>
2020-03-21 14:03:00 +0800

    new notofication page.java

---
+++ app/src/main/java/tw/com/bussinessmeet/NotificationActivity.java
@@ -0,0 +1,14 @@
+package tw.com.bussinessmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
