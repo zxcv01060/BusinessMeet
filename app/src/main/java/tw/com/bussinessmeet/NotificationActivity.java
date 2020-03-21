122188108498b3f498d912fe98dd40b63b7c5a30
Alice Lee <alice880405@gmail.com>
2020-03-21 21:03:45 +0800

    notification verson2.0

--- app/src/main/java/tw/com/bussinessmeet/NotificationActivity.java
+++ app/src/main/java/tw/com/bussinessmeet/NotificationActivity.java
@@ -3,59 +3,54 @@ package tw.com.bussinessmeet;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.content.Context;
+
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
 
+    private Intent intent;
+    private Button button;
     private static final int NOTIFICATION_ID = 0;
-    Button btNotification;
+    private NotificationManager notificationManger;
+    private Notification notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
+        ///
 
-        btNotification = (Button)findViewById(R.id.bt_notification);
+        button = (Button) findViewById(R.id.bt_notification);
+        notificationManger = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        btNotification.setOnClickListener(new View.OnClickListener() {
+        intent = new Intent();
+        intent.setClass(NotificationActivity.this, FriendsIntroductionActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            notification = new Notification.Builder(this)
+                    .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                    .setContentTitle("Hi")
+                    .setContentText("Nice to meet you.")
+                    .setContentIntent(pendingIntent)
+                    .build(); // available from API level 11 and onwards
+        }
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String message = "This is a.";
-                //定義訊息
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                        NotificationActivity.this
-                )
-                        .setSmallIcon(R.drawable.ic_insert_comment_black_24dp)  //icon
-                        .setContentText("New Notification") //標題
-                        .setContentText(message)  //內容
-                        .setAutoCancel(true);
-                //宣告Intent物件 跳至friends_introduction
-                Intent intent = new Intent(NotificationActivity.this,
-                        FriendsIntroductionActivity.class);
-
-
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.putExtra("message",message);
-
-
-                // 宣告一個 PendingIntent 的物件(執行完並不會馬上啟動,點訊息的時候才會跳到別的 Activity)
-                PendingIntent pendingIntent = PendingIntent.getActivity(NotificationActivity.this,
-                        NOTIFICATION_ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
-                builder.setContentIntent(pendingIntent);
-
-                //定義一個訊息管理者 和系統要 取得訊息管理者的物件
-                NotificationManager notificationManager = (NotificationManager)getSystemService(
-                        Context.NOTIFICATION_SERVICE
-                );
-
-                //要求傳送一個訊息
-                notificationManager.notify(0,builder.build());
+            public void onClick(View v) {
+                notificationManger.notify(0, notification);
             }
         });
     }
