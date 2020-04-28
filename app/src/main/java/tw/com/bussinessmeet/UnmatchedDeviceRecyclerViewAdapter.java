ef8798fc4203367533513b51e8a1d9203fb0d335
Wen Kao <u.k23@outlook.com>
2020-04-28 22:37:22 +0800

    未配對藍芽清單顯示姓名和頭貼(未解決多個重複和不是會員的藍芽清單)

--- app/src/main/java/tw/com/bussinessmeet/UnmatchedDeviceRecyclerViewAdapter.java
+++ app/src/main/java/tw/com/bussinessmeet/UnmatchedDeviceRecyclerViewAdapter.java
@@ -1,31 +1,74 @@
 package tw.com.bussinessmeet;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import tw.com.bussinessmeet.helper.AvatarHelper;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Iterator;
 import java.util.List;
 
+import retrofit2.Call;
+import tw.com.bussinessmeet.bean.Empty;
+import tw.com.bussinessmeet.bean.ResponseBody;
 import tw.com.bussinessmeet.bean.UserInformationBean;
+import tw.com.bussinessmeet.dao.UserInformationDAO;
+import tw.com.bussinessmeet.helper.AsyncTasKHelper;
+import tw.com.bussinessmeet.service.Impl.UserInformationServiceImpl;
 
 
 public class UnmatchedDeviceRecyclerViewAdapter extends RecyclerView.Adapter<UnmatchedDeviceRecyclerViewAdapter.ViewHolder> {
     private LayoutInflater layoutInflater;
     private Context context;
-    private  List<UserInformationBean> userInformationBeanList;
+    private String un;
+    private Bitmap avatar;
+    private UserInformationDAO userInformationDAO;
+    private List<UserInformationBean> userInformationBeanList;
+    private UserInformationBean userInformationBean;
     private MatchedClickListener matchedClickListener;
+    private AvatarHelper avatarHelper;
+
     UnmatchedDeviceRecyclerViewAdapter(Context context, List<UserInformationBean> userInformationBeanList) {
         this.layoutInflater = LayoutInflater.from(context);
         this.context = context;
         this.userInformationBeanList = userInformationBeanList;
     }
+
+    private UserInformationBean ufb;
+    private UserInformationServiceImpl userInformationApi;
+    private AsyncTasKHelper.OnResponseListener<UserInformationBean, List<UserInformationBean>> searchBluetoothResponseListener =
+            new AsyncTasKHelper.OnResponseListener<UserInformationBean, List<UserInformationBean>>() {
+                @Override
+                public Call<ResponseBody<List<UserInformationBean>>> request(UserInformationBean... userInformationBeans) {
+                    return userInformationApi.search(userInformationBeans[0]);
+                }
+
+                @Override
+                public void onSuccess(List<UserInformationBean> userInformationBeanList) {
+                    System.out.println("List" + userInformationBeanList.size());
+                    if (userInformationBeanList.size() != 0) {
+                        ufb = userInformationBeanList.get(0);
+                        un = ufb.getUserName();
+                        avatarHelper = new AvatarHelper();
+                        avatar = avatarHelper.getImageResource(ufb.getAvatar());
+                        Log.d("unmatched-username", un);
+                    }
+                }
+
+                @Override
+                public void onFail(int status) {
+
+                }
+            };
 
     @NonNull
     @Override
@@ -36,9 +79,9 @@ public class UnmatchedDeviceRecyclerViewAdapter extends RecyclerView.Adapter<Unm
 
     @Override
     public void onBindViewHolder(@NonNull UnmatchedDeviceRecyclerViewAdapter.ViewHolder holder, int position) {
-        UserInformationBean ufb = userInformationBeanList.get(position);
-        holder.bindInformation(ufb.getBlueTooth(),ufb.getAvatar());
-
+        //UserInformationBean ufb = userInformationBeanList.get(position);
+        ufb = userInformationBeanList.get(position);
+        holder.bindInformation(un, avatar);
     }
 
     @Override
@@ -52,7 +95,6 @@ public class UnmatchedDeviceRecyclerViewAdapter extends RecyclerView.Adapter<Unm
         TextView search_name;
 
 
-
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             search_pro_pic_small = itemView.findViewById(R.id.search_pro_pic_small);
@@ -60,34 +102,38 @@ public class UnmatchedDeviceRecyclerViewAdapter extends RecyclerView.Adapter<Unm
             itemView.setOnClickListener(this);
         }
 
-        void bindInformation(String userName, String avatar){
+        void bindInformation(String userName, Bitmap avatar) {
             search_name.setText(userName);
-
+            search_pro_pic_small.setImageBitmap(avatar);
         }
 
         @Override
         public void onClick(View v) {
-            Log.d("results","unmatched1");
-                if(matchedClickListener != null){
-                    Log.d("results","unmatched");
-                    matchedClickListener.onMatchedClick(v,getAdapterPosition());
-                }
+            if (matchedClickListener != null) {
+                matchedClickListener.onMatchedClick(v, getAdapterPosition());
+            }
         }
 
     }
-    void  setClickListener(MatchedClickListener searchClickLinster){
+
+    void setClickListener(MatchedClickListener searchClickLinster) {
         this.matchedClickListener = searchClickLinster;
     }
-    public UserInformationBean getUserInformation(int position){
+
+    public UserInformationBean getUserInformation(int position) {
         return userInformationBeanList.get(position);
     }
-    public void dataInsert(UserInformationBean userInformationBean){
-        Log.d("resultDataInsert",userInformationBean.getBlueTooth());
+
+    public void dataInsert(UserInformationBean userInformationBean) {
+        Log.d("resultDataInsert", userInformationBean.getBlueTooth());
         userInformationBeanList.add(userInformationBean);
+        AsyncTasKHelper.execute(searchBluetoothResponseListener, userInformationBean);
         notifyItemInserted(getItemCount());
     }
-    public interface MatchedClickListener{
+
+    public interface MatchedClickListener {
         void onMatchedClick(View view, int position);
     }
+
 
 }
