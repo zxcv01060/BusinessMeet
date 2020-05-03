605803c0301020a18f705b38c6a599a6c346ef61
Alice Lee <alice880405@gmail.com>
2020-05-03 10:30:15 +0800

    add log.d and need to test from BThelper

--- app/src/main/java/tw/com/bussinessmeet/helper/BlueToothHelper.java
+++ app/src/main/java/tw/com/bussinessmeet/helper/BlueToothHelper.java
@@ -195,6 +195,7 @@ public class BlueToothHelper {
                 Log.d("into",action);
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // 從intent中獲取裝置
+                Log.d("intocode",action);
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 String aa = "";
                 Log.d("intomatch",device.getAddress());
@@ -380,101 +381,6 @@ public class BlueToothHelper {
         }
     }
 
-    public void sendMessage() {
-
-        //avatar = (ImageView) findViewById(R.id.edit_person_photo);
-        AvatarHelper avatarHelper = new AvatarHelper();
-        Log.d("seedmess","ness");
-        UserInformationBean ufb = new UserInformationBean();
-        ufb.setBlueTooth(getMyBuleTooth());
-        Cursor result = userInformationDAO.searchAll(ufb);
-        result.moveToFirst();
-
-        //Bitmap profilePhoto = avatar.setImageBitmap(avatarHelper.getImageResource(result.getString(result.getColumnIndex("avatar"))));
-        String userName;
-
-        userName = "邱殷龍";
-        String title1 = userName;
-        String message1 = "原音股份有限公司";
-        String title2 = "李赫宰";
-        String message2 = " SM娛樂公司";
-
-
-
-        NotificationCompat.Builder notification1 = new NotificationCompat.Builder(
-                activity, CHANNEL_1_ID
-        )
-                .setSmallIcon(R.drawable.ic_insert_comment_black_24dp)
-                .setContentTitle(title1)
-                .setContentText(message1)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setAutoCancel(true)
-                .setColor(Color.rgb(4,42,88))
-                .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                .setLargeIcon(avatarHelper.getImageResource(result.getString(result.getColumnIndex("avatar"))))
-                .setGroup("group");
-
-
-        NotificationCompat.Builder notification2 = new NotificationCompat.Builder(
-                activity, CHANNEL_1_ID
-        )
-                .setSmallIcon(R.drawable.ic_insert_comment_black_24dp)
-                .setContentTitle(title2)
-                .setContentText(message2)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setAutoCancel(true)
-                .setColor(Color.rgb(4,42,88))
-                .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                .setLargeIcon(avatarHelper.getImageResource(result.getString(result.getColumnIndex("avatar"))))
-                .setGroup("group");
-        NotificationCompat.Builder summaryNotification = new NotificationCompat.Builder(
-                activity, CHANNEL_1_ID
-        )
-                .setSmallIcon(R.drawable.ic_insert_comment_black_24dp)
-                .setStyle(new NotificationCompat.InboxStyle()
-                        .addLine(title2 + " " + message2)
-                        .addLine(title1 + " " + message1)
-                        .setBigContentTitle("2 new messages"))
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setDefaults(NotificationCompat.DEFAULT_ALL) //設置鈴聲和振動 Android 7.1（API級別25）及更低版本上
-                //.setPriority(NotificationManager.IMPORTANCE_HIGH)
-                .setColor(Color.rgb(4,42,88))
-                .setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-                .setGroup("group")
-                .setColor(Color.BLUE)
-                .setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_CHILDREN)
-                .setGroupSummary(true);
-
-
-        //宣告Intent物件 跳至friends_introduction
-        Intent intent = new Intent(activity,
-                FriendsIntroductionActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra("title", title1);
-
-        // 宣告一個 PendingIntent 的物件(執行完並不會馬上啟動,點訊息的時候才會跳到別的 Activity)
-        PendingIntent pendingIntent = PendingIntent.getActivity(activity,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        notification1.setContentIntent(pendingIntent);
-        notification2.setContentIntent(pendingIntent);
-        summaryNotification.setContentIntent(pendingIntent);
-
-        //定義一個訊息管理者 和系統要 取得訊息管理者的物件
-        NotificationManager notificationManager = (NotificationManager) activity.getSystemService(
-                Context.NOTIFICATION_SERVICE
-        );
-
-        //要求傳送一個訊息
-        //id若一樣，則為更新通知，之前的通知會不見
-        SystemClock.sleep(1000);
-        notificationManager.notify(2, notification1.build());
-        SystemClock.sleep(1000);
-        notificationManager.notify(3, notification2.build());
-        SystemClock.sleep(1000);
-        notificationManager.notify(4, summaryNotification.build());
-    }
     public String getMyBuleTooth() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         String bluetoothMacAddress = "";
