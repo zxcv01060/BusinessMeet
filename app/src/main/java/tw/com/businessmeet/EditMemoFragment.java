b1ff9de54d6ecf61220b1aaa2371b39253abc3cc
Wen Kao <u.k23@outlook.com>
2020-09-01 15:48:40 +0800

    show friends' memo
    recyclerView on friends' memo

--- app/src/main/java/tw/com/businessmeet/EditMemoFragment.java
+++ app/src/main/java/tw/com/businessmeet/EditMemoFragment.java
@@ -1,14 +1,12 @@
 package tw.com.businessmeet;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,24 +14,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import retrofit2.Call;
-import tw.com.businessmeet.bean.FriendBean;
+import tw.com.businessmeet.adapter.FriendMemoAddColumnRecyclerViewAdapter;
 import tw.com.businessmeet.bean.FriendCustomizationBean;
 import tw.com.businessmeet.bean.ResponseBody;
 import tw.com.businessmeet.dao.FriendCustomizationDAO;
-import tw.com.businessmeet.dao.FriendDAO;
 import tw.com.businessmeet.helper.AsyncTasKHelper;
 import tw.com.businessmeet.helper.DBHelper;
-import tw.com.businessmeet.service.FriendCustomizationService;
 import tw.com.businessmeet.service.Impl.FriendCustomizationServiceImpl;
-import tw.com.businessmeet.service.Impl.FriendServiceImpl;
 
 
 /**
@@ -51,17 +46,23 @@ public class EditMemoFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    //floating button
+    private View view;
+
+    // floating button
     private FloatingActionButton floatingActionButton;
 
-    //dialog
+    // MemoFragment
+    private FriendCustomizationBean fcb = new FriendCustomizationBean();
+    private RecyclerView recyclerViewMemo;
+    private ArrayList<FriendCustomizationBean> friendCustomizationBeanList = new ArrayList<FriendCustomizationBean>();
+    private FriendMemoAddColumnRecyclerViewAdapter friendMemoAddColumnRecyclerViewAdapter;
+
+    // dialog
     private Button confirm, cancel;
     private EditText addColumnMemo;
-
     private FriendCustomizationDAO friendCustomizationDAO;
     private DBHelper dh = null;
     private FriendCustomizationServiceImpl friendCustomizationServiceImpl = new FriendCustomizationServiceImpl();
-    private FriendServiceImpl friendServiceImpl = new FriendServiceImpl();
     private AsyncTasKHelper.OnResponseListener<FriendCustomizationBean, FriendCustomizationBean> addResponseListener =
             new AsyncTasKHelper.OnResponseListener<FriendCustomizationBean, FriendCustomizationBean>() {
                 @Override
@@ -71,13 +72,35 @@ public class EditMemoFragment extends Fragment {
 
                 @Override
                 public void onSuccess(FriendCustomizationBean friendCustomizationBean) {
-
                 }
 
                 @Override
                 public void onFail(int status) {
                 }
             };
+
+    private AsyncTasKHelper.OnResponseListener<FriendCustomizationBean, List<FriendCustomizationBean>> searchResponseListener = new AsyncTasKHelper.OnResponseListener<FriendCustomizationBean, List<FriendCustomizationBean>>() {
+
+        @Override
+        public Call<ResponseBody<List<FriendCustomizationBean>>> request(FriendCustomizationBean... friendCustomizationBeans) {
+            return friendCustomizationServiceImpl.search(friendCustomizationBeans[0]);
+        }
+
+        @Override
+        public void onSuccess(List<FriendCustomizationBean> friendCustomizationBeans) {
+            Log.d("MemoTitle", "count:" + friendCustomizationBeans.size());
+            if (friendCustomizationBeans.size() > 1 || (friendCustomizationBeans.size() == 1 && (friendCustomizationBeans.get(0).getCreateDate() != null && !friendCustomizationBeans.get(0).equals("")))) {
+                for (int i = 0; i < friendCustomizationBeans.size(); i++) {
+                    friendCustomizationBeanList.add(friendCustomizationBeans.get(i));
+                    Log.d("TitleName", i + ":" + friendCustomizationBeanList.get(i).getName());
+                }
+            }
+        }
+
+        @Override
+        public void onFail(int status) {
+        }
+    };
 
     public EditMemoFragment() {
         // Required empty public constructor
@@ -108,18 +131,24 @@ public class EditMemoFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        fcb.setFriendNo(getActivity().getIntent().getIntExtra("friendNo", 0));
+        Log.d("fcb", "No:" + fcb.getFriendNo());
+        AsyncTasKHelper.execute(searchResponseListener, fcb);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_edit_memo, container, false);
-        //floating button
+        view = inflater.inflate(R.layout.fragment_edit_memo, container, false);
+        // recyclerView
+        recyclerViewMemo = (RecyclerView) view.findViewById(R.id.friends_edit_profile_memo_recycleView);
+        initRecyclerView();
+        // floating button
         floatingActionButton = (FloatingActionButton) view.findViewById(R.id.memo_addColumn);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //dialog
+                // dialog
                 Log.d("EditMemoFragment", "friendNo:" + getActivity().getIntent().getIntExtra("friendNo", 0));
                 LayoutInflater inflater = getActivity().getLayoutInflater();
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -133,9 +162,7 @@ public class EditMemoFragment extends Fragment {
                 confirm.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        FriendCustomizationBean fcb = new FriendCustomizationBean();
                         fcb.setName(addColumnMemo.getText().toString());
-                        FriendBean fb = new FriendBean();
                         fcb.setFriendNo(getActivity().getIntent().getIntExtra("friendNo", 0));
                         openDB();
                         friendCustomizationDAO.add(fcb);
@@ -162,5 +189,17 @@ public class EditMemoFragment extends Fragment {
     private void openDB() {
         dh = new DBHelper(getContext());
         friendCustomizationDAO = new FriendCustomizationDAO(dh);
+    }
+
+    private void initRecyclerView() {
+        // 創建adapter
+        friendMemoAddColumnRecyclerViewAdapter = new FriendMemoAddColumnRecyclerViewAdapter(getActivity(), friendCustomizationBeanList);
+        // recycleView設置adapter
+        recyclerViewMemo.setAdapter(friendMemoAddColumnRecyclerViewAdapter);
+        // 設置layoutManager，可以設置顯示效果(線性布局、grid布局、瀑布流布局)
+        // 參數:上下文、列表方向(垂直Vertical/水平Horizontal)、是否倒敘
+        recyclerViewMemo.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        // 設置item的分割線
+        recyclerViewMemo.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
     }
 }
