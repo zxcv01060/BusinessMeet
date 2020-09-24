9d9caec825de92b35524b187ebf240ed5094a455
Alice Lee <alice880405@gmail.com>
2020-09-24 22:57:45 +0800

    event add location page complete

---
+++ app/src/main/java/tw/com/businessmeet/EventAddLocationActivity.java
@@ -0,0 +1,13 @@
+package tw.com.businessmeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventAddLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_add_location);
+    }
+}
